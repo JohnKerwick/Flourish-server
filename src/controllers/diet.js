00855a56aa2ddd3d4193042fc874d77a39a35a3d
Abdulla-Khan <abdullahkhan.smiu@gmail.com>
2024-12-24 19:40:31 +0500

    Modularized Diet Controller

--- src/controllers/diet.js
+++ src/controllers/diet.js
@@ -1,82 +1,29 @@
 import { StatusCodes } from 'http-status-codes'
-const axios = require('axios')
 import dotenv from 'dotenv'
-import { calculateBMR } from '../services'
+import {
+  calculateBMR,
+  getUserById,
+  getAllMenuItems,
+  createWeeklyDietPlanService,
+  saveDietPlan,
+  getDietDetails,
+  getDietHistoryService,
+} from '../services'
 import jwt from 'jsonwebtoken'
-import mongoose from 'mongoose'
-
-dotenv.config()
-
-import { User, Restaurants, Diet } from '../models'
-
 import { asyncMiddleware } from '../middlewares'
-
+import { Diet } from '../models'
+dotenv.config()
 export const CONTROLLER_DIET = {
   getWeeklyDietPlan: asyncMiddleware(async (req, res) => {
     const token = req.headers.authorization?.split(' ')[1]
     const decoded = jwt.decode(token)
     const userId = decoded?._id
 
-    const user = await User.findByIdAndUpdate(userId, { lastActive: new Date() })
-      .select(
-        '-password -userTypes -name -email -file -role -eatOptions -appGoal -exerciseTypes -phone -refreshTokens -createdAt -updatedAt -exercise'
-      )
-      .lean()
-
-    const campus = user.student.school
-    if (!user) {
-      return res.status(StatusCodes.NOT_FOUND).json({
-        message: 'User not found.',
-      })
-    }
+    const user = await getUserById(userId)
 
     const { allergy, allergyTypes } = user
-
-    const allMenuItems = await Restaurants.aggregate([
-      { $match: { campus } },
-
-      { $unwind: '$menu' },
-      { $unwind: '$menu.items' },
-
-      {
-        $lookup: {
-          from: 'meals',
-          localField: 'menu.items',
-          foreignField: '_id',
-          as: 'mealDetails',
-        },
-      },
-      { $unwind: '$mealDetails' },
-      { $match: { 'mealDetails.nutrients.calories': { $gt: 0 } } },
-      ...(allergy
-        ? [
-            {
-              $match: {
-                'mealDetails.allergens': { $nin: allergyTypes },
-              },
-            },
-          ]
-        : []),
-      {
-        $project: {
-          mealName: '$mealDetails.name',
-          mealType: '$mealDetails.type',
-          ingredients: '$mealDetails.ingredients',
-          allergens: '$mealDetails.allergens',
-          dietaryPreferences: '$mealDetails.dietaryPreferences',
-          serving: '$mealDetails.serving',
-          calories: '$mealDetails.nutrients.calories',
-          protein: '$mealDetails.nutrients.protein',
-          fat: '$mealDetails.nutrients.fat',
-          carbohydrate: '$mealDetails.nutrients.carbohydrate',
-          restaurantName: '$name',
-          restaurantId: '$_id',
-          mealId: '$mealDetails._id',
-        },
-      },
-
-      { $sort: { 'mealDetails.nutrients.calories': -1 } },
-    ])
+    const campus = user.student.school
+    const allMenuItems = await getAllMenuItems(campus, allergy, allergyTypes)
 
     const mealItemsByType = {
       breakfast: allMenuItems.filter((item) => item.mealType === 'Breakfast'),
@@ -91,72 +38,7 @@ export const CONTROLLER_DIET = {
     }
 
     const totalCalories = calculateBMR(user)
-    const calorieDistribution = {
-      breakfast: Math.round(totalCalories * 0.3),
-      lunch: Math.round(totalCalories * 0.4),
-      dinner: Math.round(totalCalories * 0.3),
-    }
-
-    const weeklyPlan = []
-    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    const usedMeals = new Set()
-
-    for (let dayIndex = 0; dayIndex < 7; dayIndex++) {
-      const dayPlan = { day: daysOfWeek[dayIndex] }
-      let totalProvidedCalories = 0
-      let totalProtein = 0
-      let totalFat = 0
-      let totalCarbs = 0
-
-      for (const mealType of ['breakfast', 'lunch', 'dinner']) {
-        const availableMeals = sortedMealItemsByType[mealType].filter((meal) => !usedMeals.has(meal.mealName))
-
-        if (availableMeals.length === 0) {
-          dayPlan[mealType] = []
-          continue
-        }
-
-        const dailyMeals = []
-        let caloriesRemaining = calorieDistribution[mealType]
-
-        while (availableMeals.length > 0 && caloriesRemaining > 0) {
-          const meal = availableMeals.shift()
-
-          if (meal.calories <= caloriesRemaining) {
-            dailyMeals.push(meal)
-            usedMeals.add(meal.mealName)
-            caloriesRemaining -= meal.calories
-            totalProvidedCalories += meal.calories
-            totalProtein += meal.protein
-            totalFat += meal.fat
-            totalCarbs += meal.carbohydrate
-          }
-        }
-
-        dayPlan[mealType] = dailyMeals
-      }
-
-      const calorieShortfall = totalCalories - totalProvidedCalories
-      if (calorieShortfall > 0) {
-        const adjustment = Math.ceil(calorieShortfall / 3)
-        for (const mealType of ['breakfast', 'lunch', 'dinner']) {
-          if (dayPlan[mealType].length > 0) {
-            dayPlan[mealType][0].calories += adjustment
-            totalProvidedCalories += adjustment
-
-            if (totalProvidedCalories >= totalCalories) break
-          }
-        }
-      }
-
-      dayPlan.caloriesBMR = Math.trunc(totalCalories)
-      dayPlan.caloriesProvided = Math.trunc(totalProvidedCalories)
-      dayPlan.proteinProvided = Math.trunc(totalProtein)
-      dayPlan.fatProvided = Math.trunc(totalFat)
-      dayPlan.carbsProvided = Math.trunc(totalCarbs)
-
-      weeklyPlan.push(dayPlan)
-    }
+    const weeklyPlan = createWeeklyDietPlanService(totalCalories, sortedMealItemsByType)
 
     return res.status(StatusCodes.OK).json({
       message: 'Weekly diet plan generated successfully.',
@@ -167,12 +49,8 @@ export const CONTROLLER_DIET = {
 
   createWeeklyDietPlan: asyncMiddleware(async (req, res) => {
     const dietPlanData = req.body
-    let currentDate = new Date()
-    dietPlanData.dietplan.forEach((dayPlan) => {
-      dayPlan.date = new Date(currentDate)
-      currentDate.setDate(currentDate.getDate() + 1)
-    })
-    const newDietPlan = await new Diet({ ...dietPlanData }).save()
+    const newDietPlan = await saveDietPlan(dietPlanData)
+
     return res.status(StatusCodes.OK).json({
       message: 'Diet plan created successfully.',
       statusCode: StatusCodes.OK,
@@ -180,38 +58,24 @@ export const CONTROLLER_DIET = {
     })
   }),
 
-  getWeeklydietPlanDetails: asyncMiddleware(async (req, res) => {
-    const dietId = req.query.id
-    const dietPlanDetails = await Diet.findById(dietId)
-      .populate({
-        path: 'dietplan.item.meal.meal',
-        model: 'Meal',
-      })
-      .lean()
-
-    return res.status(StatusCodes.OK).json({
-      dietPlanDetails,
-      message: 'Diet plan details fetched successfully.',
-      statusCode: StatusCodes.OK,
-    })
-  }),
-
   getDietHistory: asyncMiddleware(async (req, res) => {
     const token = req.headers.authorization?.split(' ')[1]
     const decoded = jwt.decode(token)
     const userId = decoded?._id
-
-    const date = Date.now()
-    const dietPlans = await Diet.find({
-      dietOwner: userId,
-      expiresAt: { $lt: date },
-    })
-      .select('name _id')
-      .lean()
-
+    const dietPlans = await getDietHistoryService(userId)
     return res.status(StatusCodes.OK).json({
       dietPlans,
       message: 'Diet History fetched successfully.',
+      statusCode: StatusCodes.OK,
+    })
+  }),
+
+  getWeeklydietPlanDetails: asyncMiddleware(async (req, res) => {
+    const dietId = req.query.id
+    const dietPlanDetails = await getDietDetails(dietId)
+    return res.status(StatusCodes.OK).json({
+      dietPlanDetails,
+      message: 'Diet plan details fetched successfully.',
       statusCode: StatusCodes.OK,
     })
   }),
--- src/services/diet.js
+++ src/services/diet.js
@@ -1,36 +1,165 @@
+import { User, Restaurants, Diet } from '../models'
+
 export const calculateBMR = ({ dob, gender, weight, height, goal }) => {
   const birthDate = new Date(dob)
   const now = new Date()
   let age = now.getFullYear() - birthDate.getFullYear()
-
-  // Adjust for the birthday not having occurred yet this year
   if (
     now.getMonth() < birthDate.getMonth() ||
     (now.getMonth() === birthDate.getMonth() && now.getDate() < birthDate.getDate())
   ) {
     age--
   }
-
-  // Convert height from inches to centimeters
-  const heightInCm = height * 2.54
-
-  let bmr = 0
-
-  // Calculate BMR based on gender
-  if (gender.toLowerCase() === 'male') {
-    bmr = 10 * weight + 6.25 * heightInCm - 5 * age + 5
-  } else {
-    bmr = 10 * weight + 6.25 * heightInCm - 5 * age - 161
+  const HEIGHT_CONVERSION = 2.54
+  const GENDER_FACTORS = { male: 5, female: -161 }
+  const GOAL_FACTORS = {
+    'Gain Weight': 1.2,
+    'Maintain Weight': 1.0,
+    'Loss Weight': 0.8,
   }
+  const heightInCm = height * HEIGHT_CONVERSION
+  const genderFactor = GENDER_FACTORS[gender.toLowerCase()] || 0
+  const goalFactor = GOAL_FACTORS[goal] || 1.0
 
-  // Adjust BMR based on goal
-  if (goal === 'Gain Weight') {
-    bmr *= 1.2 // Multiply by 1.2 for gaining weight
-  } else if (goal === 'Maintain Weight') {
-    bmr *= 1.0 // No adjustment for maintaining weight
-  } else if (goal === 'Loss Weight') {
-    bmr *= 0.8 // Multiply by 0.8 for losing weight
-  }
+  const bmr = (10 * weight + 6.25 * heightInCm - 5 * age + genderFactor) * goalFactor
 
   return bmr
+}
+
+export const getUserById = async (userId) => {
+  const user = await User.findByIdAndUpdate(userId, { lastActive: new Date() })
+    .select(
+      '-password -userTypes -name -email -file -role -eatOptions -appGoal -exerciseTypes -phone -refreshTokens -createdAt -updatedAt -exercise'
+    )
+    .lean()
+  return user
+}
+
+export const getAllMenuItems = async (campus, allergy, allergyTypes) => {
+  return Restaurants.aggregate([
+    { $match: { campus } },
+    { $unwind: '$menu' },
+    { $unwind: '$menu.items' },
+    {
+      $lookup: {
+        from: 'meals',
+        localField: 'menu.items',
+        foreignField: '_id',
+        as: 'mealDetails',
+      },
+    },
+    { $unwind: '$mealDetails' },
+    { $match: { 'mealDetails.nutrients.calories': { $gt: 0 } } },
+    ...(allergy
+      ? [
+          {
+            $match: {
+              'mealDetails.allergens': { $nin: allergyTypes },
+            },
+          },
+        ]
+      : []),
+    {
+      $project: {
+        mealName: '$mealDetails.name',
+        mealType: '$mealDetails.type',
+        ingredients: '$mealDetails.ingredients',
+        allergens: '$mealDetails.allergens',
+        dietaryPreferences: '$mealDetails.dietaryPreferences',
+        serving: '$mealDetails.serving',
+        calories: '$mealDetails.nutrients.calories',
+        protein: '$mealDetails.nutrients.protein',
+        fat: '$mealDetails.nutrients.fat',
+        carbohydrate: '$mealDetails.nutrients.carbohydrate',
+        restaurantName: '$name',
+        restaurantId: '$_id',
+        mealId: '$mealDetails._id',
+      },
+    },
+    { $sort: { 'mealDetails.nutrients.calories': -1 } },
+  ])
+}
+
+export const createWeeklyDietPlanService = (totalCalories, sortedMealItemsByType) => {
+  const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  const calorieDistribution = {
+    breakfast: Math.round(totalCalories * 0.3),
+    lunch: Math.round(totalCalories * 0.4),
+    dinner: Math.round(totalCalories * 0.3),
+  }
+  const usedMeals = new Set()
+  const weeklyPlan = []
+
+  for (let dayIndex = 0; dayIndex < 7; dayIndex++) {
+    const dayPlan = { day: daysOfWeek[dayIndex] }
+    let totalProvidedCalories = 0
+    let totalProtein = 0
+    let totalFat = 0
+    let totalCarbs = 0
+
+    for (const mealType of ['breakfast', 'lunch', 'dinner']) {
+      const availableMeals = sortedMealItemsByType[mealType].filter((meal) => !usedMeals.has(meal.mealName))
+
+      if (availableMeals.length === 0) {
+        dayPlan[mealType] = []
+        continue
+      }
+
+      const dailyMeals = []
+      let caloriesRemaining = calorieDistribution[mealType]
+
+      while (availableMeals.length > 0 && caloriesRemaining > 0) {
+        const meal = availableMeals.shift()
+
+        if (meal.calories <= caloriesRemaining) {
+          dailyMeals.push(meal)
+          usedMeals.add(meal.mealName)
+          caloriesRemaining -= meal.calories
+          totalProvidedCalories += meal.calories
+          totalProtein += meal.protein
+          totalFat += meal.fat
+          totalCarbs += meal.carbohydrate
+        }
+      }
+
+      dayPlan[mealType] = dailyMeals
+    }
+
+    dayPlan.caloriesBMR = Math.trunc(totalCalories)
+    dayPlan.caloriesProvided = Math.trunc(totalProvidedCalories)
+    dayPlan.proteinProvided = Math.trunc(totalProtein)
+    dayPlan.fatProvided = Math.trunc(totalFat)
+    dayPlan.carbsProvided = Math.trunc(totalCarbs)
+
+    weeklyPlan.push(dayPlan)
+  }
+
+  return weeklyPlan
+}
+export const saveDietPlan = async (dietPlanData) => {
+  let currentDate = new Date()
+  dietPlanData.dietplan.forEach((dayPlan) => {
+    dayPlan.date = new Date(currentDate)
+    currentDate.setDate(currentDate.getDate() + 1)
+  })
+  return new Diet({ ...dietPlanData }).save()
+}
+
+export const getDietHistoryService = async (userId) => {
+  const date = Date.now()
+  return Diet.find({
+    dietOwner: userId,
+    expiresAt: { $lt: date },
+  })
+    .select('name')
+    .lean()
+}
+
+export const getDietDetails = async (dietId) => {
+  return Diet.findById(dietId)
+    .populate({
+      path: 'dietplan.item.meal.meal',
+      model: 'Meal',
+    })
+    .lean()
 }
