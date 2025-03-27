a5e403c3b4ccbe6da64438a6f14738d4ed746ad1
Abdulla-Khan <abdullahkhan.smiu@gmail.com>
2025-03-27 17:27:14 +0500

    cron job fix

--- src/utils/scrapper_cron.js
+++ src/utils/scrapper_cron.js
@@ -2,7 +2,7 @@ import cron from 'node-cron'
 import { CONTROLLER_SCRAPPER } from '../controllers/scrapper'
 
 const scheduleScraper = () => {
-  cron.schedule('0 0 * * *', async () => {
+  cron.schedule('0 0 0 * * *', async () => {
     CONTROLLER_SCRAPPER.scrapeAllMenus()
   })
 }
