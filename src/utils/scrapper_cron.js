5df97b150e17472661a8a72526796676aa608a08
Abdulla-Khan <abdullahkhan.smiu@gmail.com>
2025-04-15 17:17:31 +0500

    scrapper cron updated

--- src/utils/scrapper_cron.js
+++ src/utils/scrapper_cron.js
@@ -1,9 +1,38 @@
 import cron from 'node-cron'
-import { CONTROLLER_SCRAPPER } from '../controllers/scrapper'
+import { scrapeHPU } from '../scrappers/hpuScrapper'
+import { scrapeUMD } from '../scrappers/umdScrapper'
+import { scrapeUNCC } from '../scrappers/unccScrapper'
+
+// "0 0 * * 0", Every sunday at 00:00 - Required
+// "59 14 * * 1", Every monday at 14:59
+// "* * * * * *", Every second
+// "* * * * *", Every minute
+// 0 0 0 * * *, Every Midnight
+// 0 0 * * *, every 24 hour
 
 const scheduleScraper = () => {
-  cron.schedule('0 0 0 * * *', async () => {
-    CONTROLLER_SCRAPPER.scrapeAllMenus()
+  cron.schedule('* * * * *', async () => {
+    try {
+      const allData = []
+
+      console.log('Scraping HPU...')
+      const hpuData = await scrapeHPU()
+      if (hpuData) allData.push(...hpuData)
+
+      console.log('Scraping UNCC...')
+      const unccData = await scrapeUNCC()
+      if (unccData) allData.push(...unccData)
+
+      console.log('Scraping UMD...')
+      const umdData = await scrapeUMD()
+      if (umdData) allData.push(...umdData)
+
+      notifyError('Scrapper Sucess')
+    } catch (error) {
+      console.error('Error during scraping:', error)
+      notifyError(error)
+      res.status(500).json({ message: 'Scraping failed', error: error.message })
+    }
   })
 }
 
