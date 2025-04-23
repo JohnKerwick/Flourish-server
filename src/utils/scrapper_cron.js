70989dbb9803473fb9732ee7c7814a8e108103cc
Abdulla-Khan <abdullahkhan.smiu@gmail.com>
2025-04-23 18:34:54 +0500

    cron for scrapper fixed

--- src/utils/scrapper_cron.js
+++ src/utils/scrapper_cron.js
@@ -1,38 +1,18 @@
-import cron from 'node-cron'
-import { scrapeHPU } from '../scrappers/hpuScrapper'
-import { scrapeUMD } from '../scrappers/umdScrapper'
-import { scrapeUNCC } from '../scrappers/unccScrapper'
-import { CONTROLLER_SCRAPPER } from '../controllers/index'
+// Using ES6 module import syntax
+import { schedule } from 'node-cron'
+import { CONTROLLER_SCRAPPER } from '../controllers'
 // "0 0 * * 0", Every sunday at 00:00 - Required
 // "59 14 * * 1", Every monday at 14:59
 // "* * * * * *", Every second
 // "* * * * *", Every minute
 // 0 0 0 * * *, Every Midnight
 // 0 0 * * *, every 24 hour
-// 0 0 * * *, every 24 hour
+// Define the task using ES6 arrow function syntax
 
-export const scheduleScraper = cron.schedule(
+export const task = schedule(
   '* * * * *',
-  async () => {
-    // CONTROLLER_SCRAPPER.scrapeAllMenus()
-    console.log('ME CHAL RHA HO')
+  () => {
+    CONTROLLER_SCRAPPER.scrapeAllMenus()
   },
   { timezone: 'America/New_York' }
 )
-// try {
-//   const allData = []
-//   console.log('Scraping HPU...')
-//   const hpuData = await scrapeHPU()
-//   if (hpuData) allData.push(...hpuData)
-//   console.log('Scraping UNCC...')
-//   const unccData = await scrapeUNCC()
-//   if (unccData) allData.push(...unccData)
-//   console.log('Scraping UMD...')
-//   const umdData = await scrapeUMD()
-//   if (umdData) allData.push(...umdData)
-//   notifyError('Scrapper Sucess')
-// } catch (error) {
-//   console.error('Error during scraping:', error)
-//   notifyError(error)
-//   res.status(500).json({ message: 'Scraping failed', error: error.message })
-// }
