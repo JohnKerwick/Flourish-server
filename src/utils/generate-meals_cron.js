8202dc00a209a21d8ed328ebe6f0c16515fd8934
dojosgithub <dojo.developments@gmail.com>
2025-07-03 12:41:46 +0500

    cron changes

--- src/utils/generate-meals_cron.js
+++ src/utils/generate-meals_cron.js
@@ -31,8 +31,8 @@ const calorieRanges = {
   for (const campus of campuses) {
     for (const type of mealTypes) {
       const calorieRange = calorieRanges[type]
-
-      const totalMinutes = 15 + jobIndex * 3
+      const baseMinutes = 30 // Start at 00:30
+      const totalMinutes = baseMinutes + jobIndex * 6
       const minute = totalMinutes % 60
       const hour = Math.floor(totalMinutes / 60)
 
@@ -50,7 +50,6 @@ const calorieRanges = {
           try {
             if (count === 0) {
               // should be new GeneratedMealNew
-              await CONTROLLER_SCRAPPER.scrapeAllMenus()
               console.log('🧹 Deleting old generated meals...')
               await GeneratedMeal.deleteMany({})
               console.log('✅ Old meals deleted.')
